fc6dda2b0da43a8ba1f6a7d6aa334fbb962e1929
omsingh9860 <65703056+omsingh9860@users.noreply.github.com>
2025-04-25 00:59:21 +0530

    Create nameinvite.py

---
+++ nameinvite.py
@@ -0,0 +1,9 @@
+
+name = input("Please enter your name: ")
+
+
+def welcome_to_google_cloud(user_name):
+    print(f"Welcome to Google Cloud, {user_name}!")
+    print(f"You are now accessing GitHub using Google Cloud's powerful infrastructure.")
+
+welcome_to_google_cloud(name)
