6150b6df85905335347104d1656269167822bfb1
omsingh9860 <65703056+omsingh9860@users.noreply.github.com>
2025-04-25 00:58:44 +0530

    Create simpleinvite.py

---
+++ simpleinvite.py
@@ -0,0 +1,6 @@
+def welcome_to_google_cloud():
+    print("Welcome to Google Cloud!")
+    print("You are now accessing GitHub using Google Cloud's powerful infrastructure.")
+
+
+welcome_to_google_cloud()
